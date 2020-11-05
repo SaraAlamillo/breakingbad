9482eea3a2af20fa9683839ff7d40cf8d71579ad
Sara Alamillo Arroyo <salamillo@atsistemas.com>
2020-11-05 15:17:13 +0100

    feat: Converted characters page to function component

--- src/pages/Characters.page.jsx
+++ src/pages/Characters.page.jsx
@@ -5,139 +5,72 @@ import {
   ListItemAvatar,
   TextField,
 } from "@material-ui/core";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Link, withRouter, Route } from "react-router-dom";
 import { DATA_CALL_CHARACTERS } from "../actions";
 import { CharacterRedux } from "./Character.page";
+import { contentData, sortArray } from "../utils";
 
-export class Characters extends Component {
-  static propTypes = {};
-
-  static defaultProps = {};
-
-  static mapStateToProps = (state) => ({ ...state });
-
-  static mapDispatchToProps = (dispatch) => ({
-    getCharacters: () => dispatch({ type: DATA_CALL_CHARACTERS }),
-  });
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      characters: [],
-    };
+export const Characters = ({ loading, dataCharacters, getCharacters }) => {
+  if (!loading && !contentData(dataCharacters)) {
+    getCharacters();
   }
 
-  sort(characters) {
-    return characters.sort((character1, character2) =>
-      character1.name > character2.name ? 1 : -1
+  const [filteredData, setFilteredData] = useState(dataCharacters);
+
+  const handleChangeSearch = (event) => {
+    const value = event.target.value.toLowerCase();
+
+    setFilteredData(
+      value !== ""
+        ? sortArray(dataCharacters, "name").filter((character) =>
+            character.name.toLowerCase().includes(value)
+          )
+        : sortArray(dataCharacters, "name")
     );
-  }
+  };
 
-  callGetCharacters(props, nextProps) {
-    if (
-      !props.loading &&
-      !nextProps.loading &&
-      (!Array.isArray(props.dataCharacters) ||
-        props.dataCharacters.length === 0)
-    ) {
-      nextProps.getCharacters();
-    }
-  }
+  return (
+    <>
+      <Route path="/characters/:name" component={CharacterRedux}></Route>
 
-  componentDidMount() {
-    this.callGetCharacters(this.props, this.props);
-  }
+      <form>
+        <TextField
+          id="characters-search"
+          label="Search in characters"
+          onChange={handleChangeSearch}
+        />
+      </form>
 
-  shouldComponentUpdate(nextProps, nextState) {
-    let render = false;
+      <Grid>
+        {filteredData.map((character) => (
+          <Button
+            key={character.char_id}
+            component={Link}
+            to={"/characters/" + character.name}
+            style={{ margin: 20 }}
+          >
+            <ListItemAvatar>
+              <Avatar src={character.img} />
+            </ListItemAvatar>
+            {character.name}
+          </Button>
+        ))}
+      </Grid>
+    </>
+  );
+};
 
-    this.callGetCharacters(this.props, nextProps);
+const mapStateToProps = (state) => ({ ...state });
 
-    if (
-      (Array.isArray(nextProps.dataCharacters) &&
-        nextProps.dataCharacters.length > 0 &&
-        Array.isArray(this.props.dataCharacters) &&
-        this.props.dataCharacters.length > 0 &&
-        nextProps.dataCharacters.length !== this.props.dataCharacters.length) ||
-      (Array.isArray(nextProps.dataCharacters) &&
-        nextProps.dataCharacters.length > 0)
-    ) {
-      render = true;
-    }
-
-    return render;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      (Array.isArray(prevProps.dataCharacters) &&
-        prevProps.dataCharacters.length > 0 &&
-        Array.isArray(this.props.dataCharacters) &&
-        this.props.dataCharacters.length > 0 &&
-        prevProps.dataCharacters.length !== this.props.dataCharacters.length &&
-        this.props.dataCharacters.length !== this.state.characters.length) ||
-      (Array.isArray(this.props.dataCharacters) &&
-        this.props.dataCharacters.length > 0 &&
-        this.props.dataCharacters.length !== this.state.characters.length)
-    ) {
-      this.setState((state, props) => ({
-        characters: this.sort(props.dataCharacters),
-      }));
-    }
-  }
-
-  render() {
-    const handleChangeSearch = (event) => {
-      const value = event.target.value.toLowerCase();
-
-      this.setState((state, props) => ({
-        characters:
-          value !== ""
-            ? this.sort(props.dataCharacters).filter((character) =>
-                character.name.toLowerCase().includes(value)
-              )
-            : this.sort(props.dataCharacters),
-      }));
-    };
-
-    return (
-      <>
-        <Route path="/characters/:name" component={CharacterRedux}></Route>
-
-        <form>
-          <TextField
-            id="characters-search"
-            label="Search in characters"
-            onChange={handleChangeSearch}
-          />
-        </form>
-
-        <Grid>
-          {this.state.characters.map((character) => (
-            <Button
-              key={character.char_id}
-              component={Link}
-              to={"/characters/" + character.name}
-              style={{ margin: 20 }}
-            >
-              <ListItemAvatar>
-                <Avatar src={character.img} />
-              </ListItemAvatar>
-              {character.name}
-            </Button>
-          ))}
-        </Grid>
-      </>
-    );
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  getCharacters: () => dispatch({ type: DATA_CALL_CHARACTERS }),
+});
 
 export const CharactersRoute = withRouter(Characters);
 
 export const CharactersRedux = connect(
-  Characters.mapStateToProps,
-  Characters.mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(CharactersRoute);
