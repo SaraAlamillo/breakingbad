b28a51594e996b06b07dfae78301603501542863
Sara Alamillo Arroyo <salamillo@atsistemas.com>
2020-11-05 15:15:49 +0100

    feat: Added sort function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 /**
  * Comprueba si el valor pasado es un array y si contiene algo
  * @param {Any} value Valor a comprobar
@@ -5,4 +7,24 @@
  */
 export const contentData = (value) => {
   return Array.isArray(value) && value.length > 0;
+};
+
+/**
+ * Devuelve un array ordenado de forma ascendente
+ * @param {Array} list Array a ordenar
+ * @param {String} field Si es un array de objetos, será necesario especificar el campo por el que ser ordenará. Si no se indica, se utilizará toda la posición
+ * @returns {Array} Array ordenado
+ */
+export const sortArray = (list, field) => {
+  return list.sort((a, b) => {
+    const value1 = field ? a[field] : a;
+    const value2 = field ? b[field] : b;
+
+    return value1 > value2 ? 1 : -1;
+  });
+};
+
+sortArray.propTypes = {
+  list: PropTypes.array.isRequired,
+  field: PropTypes.string,
 };
