82e29c3d2bac4e5c8c4c149e4f78e81efd33f2ec
Sara Alamillo Arroyo <salamillo@atsistemas.com>
2020-11-11 13:53:37 +0100

    feat: Converted Episodes page in function component

--- src/pages/Episodes.page.jsx
+++ src/pages/Episodes.page.jsx
@@ -1,57 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { Table } from "../components/Table/Table.component";
 
-export class Episodes extends Component {
-  static propTypes = {
-    data: PropTypes.arrayOf(
-      PropTypes.shape({
-        episode_id: PropTypes.number.isRequired,
-        title: PropTypes.string.isRequired,
-        season: PropTypes.string,
-        air_date: PropTypes.string,
-        episode: PropTypes.string,
-        series: PropTypes.string,
-        characters: PropTypes.arrayOf(PropTypes.string),
-      })
-    ).isRequired,
-  };
+export const Episodes = ({ data }) => {
+  const headers = [
+    { id: "episode", name: "Episode" },
+    { id: "title", name: "Title" },
+    { id: "detail", name: "See more" },
+  ];
 
-  static mapStateToProps = (state) => ({ ...state });
+  return <Table headers={headers} body={data} collapse={false}></Table>;
+};
 
-  static mapDispatchToProps = (dispatch) => ({});
+Episodes.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      episode_id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      detail: PropTypes.node.isRequired,
+      episode: PropTypes.string.isRequired,
+      season: PropTypes.string,
+      air_date: PropTypes.string,
+      series: PropTypes.string,
+      characters: PropTypes.arrayOf(PropTypes.string),
+    })
+  ).isRequired,
+};
 
-  constructor(props) {
-    super(props);
+const mapStateToProps = (state) => ({ ...state });
 
-    this.state = {
-      episodes: props.data,
-    };
-  }
-
-  render() {
-    const headers = [
-      { id: "episode", name: "Episode" },
-      { id: "title", name: "Title" },
-      { id: "detail", name: "See more" },
-    ];
-    return (
-      <Table
-        headers={headers}
-        body={this.state.episodes}
-        collapse={false}
-      ></Table>
-    );
-  }
-
-  componentDidMount() {}
-}
+const mapDispatchToProps = (dispatch) => ({});
 
 export const EpisodesRoute = withRouter(Episodes);
 
 export const EpisodesRedux = connect(
-  Episodes.mapStateToProps,
-  Episodes.mapDispatchToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(EpisodesRoute);
